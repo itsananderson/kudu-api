a60d34079e69ea0b1cfd4b8df7e17871426e19ee
Will Anderson <will@itsananderson.com>
2015-07-19 22:08:57 -0700

    Improve test for dump api

--- test/dump.js
+++ test/dump.js
@@ -4,14 +4,15 @@ var path = require("path");
 var api = require("../")(process.env.WEBSITE, process.env.USERNAME, process.env.PASSWORD);
 
 describe("dump", function() {
-    this.timeout(5000);
+    this.timeout(30 * 1000);
 
     it("can retrieve dump", function(done) {
         var dest = path.join(__dirname, "test.zip");
         if (fs.existsSync(dest)) {
             fs.unlinkSync(dest);
         }
-        api.dump.download(dest, function() {
+        api.dump.download(dest, function(err) {
+            if (err) done(err);
             assert(fs.existsSync(dest), "Downloaded dump file exists");
             done();
         });
