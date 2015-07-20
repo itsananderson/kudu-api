a6a084560d447bb593e7912903ff37fc0ae7bb7e
Will Anderson <will@itsananderson.com>
2015-07-19 22:25:23 -0700

    Improve tests for logs api

--- test/logs.js
+++ test/logs.js
@@ -1,16 +1,22 @@
+var assert = require("assert");
 var api = require("../")(process.env.WEBSITE, process.env.USERNAME, process.env.PASSWORD);
 
 describe("logs", function() {
     this.timeout(5000);
 
     it("can retrieve recent logs", function(done) {
-        api.logs.recent(function() {
+        api.logs.recent(function(err, logs) {
+            if (err) done(err);
+            
+            assert(Array.isArray(logs), "logs should be an array");
             done();
         });
     });
 
     it("can retrieve custom number of recent logs", function(done) {
-        api.logs.recent(500, function() {
+        api.logs.recent(500, function(err, logs) {
+            if (err) done(err);
+            assert(Array.isArray(logs), "logs should be an array");
             done();
         });
     });
