c4805a197cacd6e50fb7dfdc9cab97de23f4de94
Will Anderson <will@itsananderson.com>
2015-07-19 22:43:43 -0700

    Improve tests for settings api

--- test/settings.js
+++ test/settings.js
@@ -10,20 +10,26 @@ describe("settings", function() {
 
     it("can retrieve all settings", function(done) {
         api.settings.list(function(err, settings) {
+            if (err) done(err);
+            assert.equal(settings.WEBSITE_SITE_NAME, process.env.WEBSITE, "Website sitname should match");
             done();
         });
     });
     it("can retrieve a single setting", function(done) {
         api.settings.get("WEBSITE_SITE_NAME", function(err, setting) {
+            if (err) done(err);
             assert.equal(setting, process.env.WEBSITE);
             done();
         });
     });
     it("can update settings", function(done) {
         api.settings.get("test_setting", function(err, setting) {
+            if (err) done(err);
             assert.equal(setting, "test");
             api.settings.set({test_setting: "test1"}, function(err) {
+                if (err) done(err);
                 api.settings.get("test_setting", function(err, setting) {
+                    if (err) done(err);
                     assert.equal(setting, "test1");
                     done();
                 });
@@ -31,8 +37,20 @@ describe("settings", function() {
         });
     });
     it("can delete a setting", function(done) {
-        api.settings.del("test_setting", function(err) {
-            done();
+        this.timeout(10 * 1000);
+        api.settings.set({test_setting: "test"}, function(err) {
+            if (err) done(err);
+            api.settings.list(function(err, oldSettings) {
+                if (err) done(err);
+                api.settings.del("test_setting", function(err) {
+                    api.settings.list(function(err, newSettings) {
+                        var oldKeys = Object.keys(oldSettings);
+                        var newKeys = Object.keys(newSettings);
+                        assert.equal(newKeys.length, oldKeys.length-1, "New keys count should be old count minus 1");
+                        done();
+                    });
+                });
+            });
         });
     });
 });
