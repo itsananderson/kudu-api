b907dc55c249ccc55e5c162c3da8efba64ec64d4
Will Anderson <will@itsananderson.com>
2015-07-19 20:07:11 -0700

    Up the timeout on the command test

--- test/command.js
+++ test/command.js
@@ -1,9 +1,8 @@
 var api = require("../")(process.env.WEBSITE, process.env.USERNAME, process.env.PASSWORD);
 
 describe("command", function() {
-    this.timeout(5000);
-
     it("can execute a command", function(done) {
+        this.timeout(30000);
         api.command.exec("echo hello world", "site", function() {
             done();
         });
