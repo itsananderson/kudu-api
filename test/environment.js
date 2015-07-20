83c51171149b0d509b9af3b6f884674f0e3e109a
Will Anderson <will@itsananderson.com>
2015-07-19 22:14:09 -0700

    Improve tests for environment api

--- test/environment.js
+++ test/environment.js
@@ -1,3 +1,4 @@
+var assert = require("assert");
 var api = require("../")(process.env.WEBSITE, process.env.USERNAME, process.env.PASSWORD);
 
 describe("environment", function() {
@@ -5,6 +6,8 @@ describe("environment", function() {
 
     it("can get the environment", function(done) {
         api.environment.get(function(err, environment) {
+            if (err) done(err);
+            assert.notStrictEqual(environment.version, undefined, "version is defined");
             done();
         });
     });
