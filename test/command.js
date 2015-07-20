757eba99886cdde13606acdea4ecad46d15696b0
Will Anderson <will@itsananderson.com>
2015-07-19 20:53:17 -0700

    Improve test for command api

--- test/command.js
+++ test/command.js
@@ -1,9 +1,15 @@
+var assert = require("assert");
 var api = require("../")(process.env.WEBSITE, process.env.USERNAME, process.env.PASSWORD);
 
 describe("command", function() {
     it("can execute a command", function(done) {
         this.timeout(30000);
-        api.command.exec("echo hello world", "site", function() {
+        api.command.exec("echo hello world", "site", function(err, result) {
+            if (err) done(err);
+
+            assert.equal(result.Error, "", "Error should be empty");
+            assert.equal(result.Output, "hello world\r\n", "Output should be 'hello world\\r\\n'");
+            assert.equal(result.ExitCode, 0, "Exit code should be 0");
             done();
         });
     });
