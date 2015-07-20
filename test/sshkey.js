7b1f8cf36026f025dfed5f024dcb3ddda75fc304
Will Anderson <will@itsananderson.com>
2015-07-19 22:47:30 -0700

    Improve tests for sshkey api

--- test/sshkey.js
+++ test/sshkey.js
@@ -1,3 +1,4 @@
+var assert = require("assert");
 var path = require("path");
 var api = require("../")(process.env.WEBSITE, process.env.USERNAME, process.env.PASSWORD);
 
@@ -5,10 +6,16 @@ describe("sshkey", function() {
     this.timeout(5000);
 
     it("can generate a new key if none exists", function(done) {
-        api.sshkey.get(true, done);
+        api.sshkey.get(true, function(err, key) {
+            assert(/^ssh-rsa/.test(key), "Should be a valid key");
+            done(err);
+        });
     });
 
     it("can download key", function(done) {
-        api.sshkey.get(done);
+        api.sshkey.get(function(err, key) {
+            assert(/^ssh-rsa/.test(key), "Should be a valid key");
+            done(err);
+        });
     });
 });
