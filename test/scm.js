9de91f73a4916e0f73b4fb59f44ad247719d6dac
Will Anderson <will@itsananderson.com>
2015-07-19 22:36:21 -0700

    Improve tests for scm api

--- test/scm.js
+++ test/scm.js
@@ -1,12 +1,20 @@
+var assert = require("assert");
 var api = require("../")(process.env.WEBSITE, process.env.USERNAME, process.env.PASSWORD);
 
 describe("scm", function() {
     this.timeout(5000);
 
     it("can retrieve info", function(done) {
-        api.scm.info(done);
+        api.scm.info(function(err, info) {
+            if (err) done(err);
+            assert.notEqual(info.Type, undefined, "Type is defined");
+            assert.notEqual(info.GitUrl, undefined, "GitUrl is defined");
+            done();
+        });
     });
     it("can clean repo", function(done) {
+        // Not much to test here
+        // If successful, returns a 204. If no Git repo exists, returns a 500
         api.scm.clean(done);
     });
 });
