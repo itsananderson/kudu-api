3e0a456ffaf5e8467d1d8053075ba9218eb1b5e0
Will Anderson <will@itsananderson.com>
2015-07-19 21:44:54 -0700

    Improve tests for the deployment api

--- test/deployment.js
+++ test/deployment.js
@@ -1,10 +1,11 @@
+var assert = require("assert");
 var fs = require("fs");
 var path = require("path");
-var api = require("../")(process.env.WEBSITE, process.env.USERNAME, process.env.PASSWORD);
 var exec = require("child_process").exec;
+var api = require("../")(process.env.WEBSITE, process.env.USERNAME, process.env.PASSWORD);
 
-var gitUrl1 = "https://github.com/itsananderson/itsananderson.github.io.git";
-var gitUrl2 = "https://github.com/itsananderson/itsananderson.github.io.git#402dce250fc649e52b201aaca24de08c353dac75";
+var gitUrl1 = "https://github.com/itsananderson/kudu-api-website.git";
+var gitUrl2 = "https://github.com/itsananderson/kudu-api-website.git#1f19ea3b8e68397b6f7c290378526ef37975105d";
 
 describe("deployment", function() {
     this.timeout(5000);
@@ -14,8 +15,11 @@ describe("deployment", function() {
     before(function(done) {
         this.timeout(30 * 1000);
         api.deployment.deploy(gitUrl1, function(err, result) {
+            if (err) done(err);
             api.deployment.deploy(gitUrl2, function(err, result) {
+                if (err) done(err);
                 api.deployment.list(function(err, deployments) {
+                    if (err) done(err);
                     deploymentList = deployments; 
                     done();
                 });
@@ -24,29 +28,57 @@ describe("deployment", function() {
     });
 
     it("can list all deployments", function(done) {
-        done();
+        api.deployment.list(function(err, deployments) {
+            if (err) done(err);
+
+            assert(Array.isArray(deployments), "Deployments should be an array");
+            done();
+        });
     });
     it("can get a single deployment", function(done) {
         api.deployment.get(deploymentList[0].id, function(err, deployment) {
+            if (err) done(err);
+
+            assert.equal(deployment.id, deploymentList[0].id, "Deployment id should match the one queried");
             done();
         });
     });
     it("can deploy a previous deployment", function(done) {
         this.timeout(30 * 1000);
         api.deployment.redeploy(deploymentList[0].id, function(err, result) {
-            done();
+            if (err) done(err);
+            api.deployment.get(deploymentList[0].id, function(err, deployment) {
+                if (err) done(err);
+
+                assert.equal(deployment.id, deploymentList[0].id, "Deployment id should match the one queried");
+                assert(deployment.active, "Deployment should be active");
+                done();
+            });
         });
     });
     it("can delete a deployment", function(done) {
-        var deploymentId = deploymentList.filter(function(d) {
-            return !d.active;
-        })[0].id;
-        api.deployment.del(deploymentId, function(err, result) {
-            done();
+        api.deployment.list(function(err, oldDeployments) {
+            if (err) done(err);
+
+            var deploymentId = oldDeployments.filter(function(d) {
+                return !d.active;
+            })[0].id;
+
+            api.deployment.del(deploymentId, function(err, result) {
+                if (err) done(err);
+                api.deployment.list(function(err, newDeployments) {
+                    if (err) done(err);
+                    assert.equal(newDeployments.length, oldDeployments.length-1, "Deployment count should be one less after deletion");
+                    done();
+                });
+            });
         });
     });
     it("can get a deployment log", function(done) {
         api.deployment.log(deploymentList[0].id, function(err, entries) {
+            if (err) done(err);
+
+            assert(Array.isArray(entries), "number", "Deployment log entries should be an array"); 
             done();
         });
     });
