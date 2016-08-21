8196e883ce2e7c177c50a5f1ece42a388937708e
Ben Quarmby <ben@quarm.by>
2016-08-21 12:39:29 +1000

    Handle HTTP errors in zip module.

--- test/zip.js
+++ test/zip.js
@@ -1,38 +1,56 @@
+"use strict";
+
 var assert = require("assert");
 var fs = require("fs");
 var path = require("path");
-var api = require("../")({website: process.env.WEBSITE, username: process.env.USERNAME, password: process.env.PASSWORD});
-var JSZip = require("jszip");
+var testUtils = require("./test-utils");
+var api;
 
-var localZipPath = path.join(__dirname, "test.zip");
+var localZipPath = path.join("test", "artifacts", "test.zip");
 
-describe("zip", function() {
+describe("zip", function () {
     this.timeout(5000);
 
-    it("can upload a zip", function(done) {
-        fs.unlink(localZipPath, function() {
-            var zip = new JSZip();
-            zip.file(path.join(__dirname, "..", "package.json"));
-            zip.generateNodeStream({type: "nodebuffer", streamFiles: true})
-                .pipe(fs.createWriteStream(localZipPath))
-                .on("error", done)
-                .on("finish", function() {
-                    api.zip.upload(localZipPath, "site/wwwroot", done);
-                });
-        });
-    });
+    before(testUtils.setupKudu(function (kuduApi) {
+        api = kuduApi;
+    }));
 
-    it("can download a folder as a zip", function(done) {
-        this.timeout(30 * 1000);
-        fs.unlink(localZipPath, function() {
-            assert(!fs.existsSync(localZipPath), "Local zip should not exist before download");
-            api.zip.download("site/wwwroot", localZipPath, function(err) {
+    before(testUtils.ensureArtifacts);
+
+    it("can upload a zip", function (done) {
+        fs.unlink(localZipPath, function () {
+            var localZipContents = {
+                "test.txt": "test\n"
+            };
+
+            testUtils.createZipFile(localZipPath, localZipContents, function (err) {
                 if (err) {
                     done(err);
                 }
 
-                assert(fs.existsSync(localZipPath), "Local zip should exist after download");
-                done();
+                api.zip.upload(localZipPath, "site/wwwroot", done);
+            });
+        });
+    });
+
+    it("can download a folder as a zip", function (done) {
+        this.timeout(30 * 1000);
+
+        fs.unlink(localZipPath, function () {
+            fs.exists(localZipPath, function (exists) {
+                assert(!exists, "Local zip should not exist before download");
+
+                api.zip.download("site/wwwroot", localZipPath, function (err) {
+                    if (err) {
+                        done(err);
+                    }
+
+                    fs.exists(localZipPath, function (exists) {
+                        assert(exists, "Local zip should exist after download");
+
+                        done();
+                    });
+                });
             });
         });
     });
