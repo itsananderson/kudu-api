b3a9398984d282c408fec066a2c02c0095d8a69e
Ben Quarmby <ben@quarm.by>
2016-08-21 13:15:32 +1000

    Zip HTTP error tests.

--- test/zip.js
+++ test/zip.js
@@ -8,6 +8,13 @@ var api;
 
 var localZipPath = path.join("test", "artifacts", "test.zip");
 
+function deleteLocalZip(done) {
+    fs.unlink(localZipPath, function () {
+        // Ignore errors
+        done();
+    });
+}
+
 describe("zip", function () {
     this.timeout(5000);
 
@@ -17,18 +24,38 @@ describe("zip", function () {
 
     before(testUtils.ensureArtifacts);
 
+    beforeEach(deleteLocalZip);
+
+    afterEach(deleteLocalZip);
+
     it("can upload a zip", function (done) {
-        fs.unlink(localZipPath, function () {
-            var localZipContents = {
-                "test.txt": "test\n"
-            };
+        var localZipContents = {
+            "test.txt": "test\n"
+        };
 
-            testUtils.createZipFile(localZipPath, localZipContents, function (err) {
-                if (err) {
-                    done(err);
-                }
+        testUtils.createZipFile(localZipPath, localZipContents, function (err) {
+            if (err) {
+                done(err);
+            }
 
-                api.zip.upload(localZipPath, "site/wwwroot", done);
+            api.zip.upload(localZipPath, "site/wwwroot", done);
+        });
+    });
+
+    it("should return a server error uploading to a folder with illegal characters", function (done) {
+        var localZipContents = {
+            "test.txt": "test\n"
+        };
+
+        testUtils.createZipFile(localZipPath, localZipContents, function (err) {
+            if (err) {
+                done(err);
+            }
+
+            api.zip.upload(localZipPath, "site/wwwroot/illegal-character\"", function (err, body, response) {
+                assert(err, "Error should exist.");
+                assert.strictEqual(err.response.statusCode, 500, "Error status code should be 500.");
+                done();
             });
         });
     });
@@ -36,22 +63,28 @@ describe("zip", function () {
     it("can download a folder as a zip", function (done) {
         this.timeout(30 * 1000);
 
-        fs.unlink(localZipPath, function () {
-            fs.exists(localZipPath, function (exists) {
-                assert(!exists, "Local zip should not exist before download");
+        fs.exists(localZipPath, function (exists) {
+            assert(!exists, "Local zip should not exist before download");
 
-                api.zip.download("site/wwwroot", localZipPath, function (err) {
-                    if (err) {
-                        done(err);
-                    }
+            api.zip.download("site/wwwroot", localZipPath, function (err) {
+                if (err) {
+                    done(err);
+                }
 
-                    fs.exists(localZipPath, function (exists) {
-                        assert(exists, "Local zip should exist after download");
+                fs.exists(localZipPath, function (exists) {
+                    assert(exists, "Local zip should exist after download");
 
-                        done();
-                    });
+                    done();
                 });
             });
+        });
+    });
+
+    it("should return a not found error downloading a non-existent folder", function (done) {
+        api.zip.download("site/wwwroot/does-not-exist", localZipPath, function (err) {
+            assert(err, "Error should exist.");
+            assert.strictEqual(err.response.statusCode, 404, "Error status code should be 404.");
+            done();
         });
     });
 });
