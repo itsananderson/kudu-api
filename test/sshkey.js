f4b88fd53c4b11f880dcf29d30db9c639ce553cf
Ben Quarmby <ben@quarm.by>
2016-08-28 21:00:33 +1000

    Handle HTTP errors in SSH key module.

--- test/sshkey.js
+++ test/sshkey.js
@@ -1,18 +1,25 @@
-var assert = require("assert");
-var api = require("../")({website: process.env.WEBSITE, username: process.env.USERNAME, password: process.env.PASSWORD});
+"use strict";
 
-describe("sshkey", function() {
+var assert = require("assert");
+var testUtils = require("./test-utils");
+var api;
+
+describe("sshkey", function () {
     this.timeout(5000);
 
-    it("can generate a new key if none exists", function(done) {
-        api.sshkey.get(true, function(err, key) {
+    before(testUtils.setupKudu(function (kuduApi) {
+        api = kuduApi;
+    }));
+
+    it("can generate a new key if none exists", function (done) {
+        api.sshkey.get(true, function (err, key) {
             assert(/^ssh-rsa/.test(key), "Should be a valid key");
             done(err);
         });
     });
 
-    it("can download key", function(done) {
-        api.sshkey.get(function(err, key) {
+    it("can download key", function (done) {
+        api.sshkey.get(function (err, key) {
             assert(/^ssh-rsa/.test(key), "Should be a valid key");
             done(err);
         });
