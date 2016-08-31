56504483c5f13104da3892d6136a743763cf8b36
Will Anderson <will@itsananderson.com>
2016-08-30 22:27:46 -0700

    Fix command.exec test for default directory

--- test/command.js
+++ test/command.js
@@ -20,7 +20,7 @@ describe("command", function() {
             if (err) done(err);
 
             assert.equal(result.Error, "", "Error should be empty");
-            assert.equal(result.Output, "hello world\r\n", "Output should be 'hello world\\r\\n'");
+            assert.equal(result.Output, "D:\\home\r\n", "Output should be 'D:\\home\\r\\n'");
             assert.equal(result.ExitCode, 0, "Exit code should be 0");
             done();
         });
