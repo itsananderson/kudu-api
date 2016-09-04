ea1bf7fbf8cf2fe1fb5382006a5243687c4f3979
Will Anderson <will@itsananderson.com>
2016-09-03 22:12:47 -0700

    Switch to eslint for linting code

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,21 @@
 
 var gulp = require("gulp");
 var mocha = require("gulp-mocha");
-var jslint = require("gulp-jslint");
+var eslint = require("gulp-eslint");
 
 var libSources = ["gulpfile.js", "lib/**/*.js"];
 var testSources = ["test/**/*.js"];
 
 gulp.task("lint-lib", function () {
     return gulp.src(libSources)
-        .pipe(jslint({
-            node: true
-        }))
-        .pipe(jslint.reporter("stylish"));
+        .pipe(eslint())
+        .pipe(eslint.format());
 });
 
 gulp.task("lint-test", function () {
-    return gulp.src(testSources).pipe(jslint({
-        options: {
-            node: true,
-            this: true
-        },
-        globals: ["describe", "it", "before", "after", "beforeEach", "afterEach"],
-        noFail: true
-    }));
+    return gulp.src(testSources)
+        .pipe(eslint())
+        .pipe(eslint.format());
 });
 
 gulp.task("lint", ["lint-lib", "lint-test"]);
