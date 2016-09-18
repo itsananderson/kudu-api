e6f0f0d9505e0437e5fcdc41c50b7793a3ff3d7a
Ben Quarmby <ben@quarm.by>
2016-09-18 13:54:53 +1000

    Merge environment tests.

--- test/environment.js
+++ test/environment.js
@@ -7,18 +7,37 @@ var api;
 describe("environment", function() {
     this.timeout(5000);
 
-    before(testUtils.setupKudu(function (kuduApi) {
-        api = kuduApi;
-    }));
+    describe("with standard credentials", function () {
+        before(testUtils.setupKudu(function (kuduApi) {
+            api = kuduApi;
+        }));
 
-    it("can get the environment", function(done) {
-        api.environment.get(function(err, environment) {
-            if (err) {
-                return done(err);
-            }
+        it("can get the environment", function(done) {
+            api.environment.get(function(err, environment) {
+                if (err) {
+                    return done(err);
+                }
 
-            assert.notStrictEqual(environment.version, undefined, "version is defined");
-            done();
+                assert.notStrictEqual(environment.version, undefined, "version is defined");
+                done();
+            });
+        });
+    });
+
+    describe("with basic credentials", function() {
+        before(testUtils.setupKudu(true, function (kuduApi) {
+            api = kuduApi;
+        }));
+
+        it("can get the environment", function(done) {
+            api.environment.get(function(err, environment) {
+                if (err) {
+                    return done(err);
+                }
+
+                assert.notStrictEqual(environment.version, undefined, "version is defined");
+                done();
+            });
         });
     });
 });
