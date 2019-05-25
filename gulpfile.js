e07ce2f6c23bbedc627ec892eca37a9737ce944f
Will Anderson <will@itsananderson.com>
2019-05-25 14:31:07 -0700

    Update Gulp config to build TypeScript

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,24 @@
 var gulp = require("gulp");
 var mocha = require("gulp-mocha");
 var eslint = require("gulp-eslint");
-
-var libSources = ["gulpfile.js", "lib/**/*.js"];
-var testSources = ["test/**/*.js"];
+var ts = require("gulp-typescript");
+var merge = require("merge2");
+var libSources = ["lib/**/*.ts"];
+var testSources = ["test/**/*.ts"];
 var allSources = libSources.concat(testSources);
+
+var builtTestFiles = ["dist/test/**/*.js"];
+
+gulp.task("build", function() {
+    var tsResult = gulp.src(allSources)
+        .pipe(ts({
+            declaration: true
+        }));
+    return merge([
+        tsResult.dts.pipe(gulp.dest('definitions')),
+        tsResult.js.pipe(gulp.dest('dist'))
+    ]);
+})
 
 gulp.task("lint", function () {
     return gulp.src(allSources)
@@ -18,10 +32,10 @@ gulp.task("watch-lint", gulp.series("lint", function () {
     return gulp.watch(allSources, ["lint"]);
 }));
 
-gulp.task("test", function () {
-    return gulp.src(testSources)
+gulp.task("test", gulp.series("build", function () {
+    return gulp.src(builtTestFiles)
         .pipe(mocha());
-});
+}));
 
 gulp.task("watch-test", gulp.series("test", function () {
     return gulp.watch(allSources, ["test"]);
