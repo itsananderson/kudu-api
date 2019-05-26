66c026588958f18b32f797c727cbae9e22da16d9
Will Anderson <will@itsananderson.com>
2019-05-25 19:25:32 -0700

    Convert 'diagnostics' API to native Promises

--- lib/diagnostics.ts
+++ lib/diagnostics.ts
@@ -1,48 +1,77 @@
 import * as utils from "./utils";
+import { ApiResponse } from "./types";
 
-export interface Diagnostics {
-  list: (cb) => void;
-  get: (key, cb) => void;
-  del: (key, cb) => void;
-  set: (settings, cb) => void;
+export type DiagnosticSettings = {
+  [key: string]: string;
+};
+
+export interface DiagnosticsApi {
+  list: () => Promise<ApiResponse<DiagnosticSettings>>;
+  get: (key: string) => Promise<ApiResponse<string>>;
+  del: (key: string) => Promise<ApiResponse<void>>;
+  set: (settings: DiagnosticSettings) => Promise<ApiResponse<void>>;
 }
 
-export default function diagnostics(request): Diagnostics {
+export default function diagnostics(request): DiagnosticsApi {
   return {
-    list: function list(cb): void {
+    list: function list(): Promise<ApiResponse<DiagnosticSettings>> {
       var options = {
         uri: "/api/diagnostics/settings",
         json: true
       };
 
-      request(options, utils.createCallback("listing diagnostic settings", cb));
+      return new Promise<ApiResponse<DiagnosticSettings>>((resolve, reject) => {
+        request(
+          options,
+          utils.createPromiseCallback(
+            "listing diagnostic settings",
+            resolve,
+            reject
+          )
+        );
+      });
     },
 
-    get: function get(key, cb): void {
+    get: function get(key: string): Promise<ApiResponse<string>> {
       var options = {
         uri: "/api/diagnostics/settings/" + encodeURIComponent(key),
         json: true
       };
-      var action = "getting diagnostic settiing with key " + key;
 
-      request(options, utils.createCallback(action, cb));
+      return new Promise<ApiResponse<string>>((resolve, reject) => {
+        request(
+          options,
+          utils.createPromiseCallback(
+            "getting diagnostic settiing with key " + key,
+            resolve,
+            reject
+          )
+        );
+      });
     },
 
-    del: function del(key, cb): void {
+    del: function del(key): Promise<ApiResponse<void>> {
       var url = "/api/diagnostics/settings/" + encodeURIComponent(key);
       var action = "deleting diagnostic settiing with key " + key;
 
-      request.del(url, utils.createCallback(action, cb));
+      return new Promise<ApiResponse<void>>((resolve, reject) => {
+        request.del(url, utils.createPromiseCallback(action, resolve, reject));
+      });
     },
 
-    set: function set(settings, cb): void {
+    set: function set(settings): Promise<ApiResponse<void>> {
       var options = {
         uri: "/api/diagnostics/settings/",
         json: settings
       };
       var action = "setting diagnostic settings";
 
-      request.post(options, utils.createCallback(action, cb));
+      return new Promise<ApiResponse<void>>((resolve, reject) => {
+        request.post(
+          options,
+          utils.createPromiseCallback(action, resolve, reject)
+        );
+      });
     }
   };
 }
