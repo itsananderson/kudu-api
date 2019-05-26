c319f22eca6f5ba3b13d25f21007af4de96f1f3f
Will Anderson <will@itsananderson.com>
2019-05-25 19:09:12 -0700

    Convert 'deployment' API to native Promises

--- test/deployment.ts
+++ test/deployment.ts
@@ -1,6 +1,7 @@
 import * as assert from "assert";
 
 import * as testUtils from "./test-utils";
+import { Deployment } from "../lib/deployment";
 
 var api;
 
@@ -11,7 +12,7 @@ var gitUrl2 =
 describe("deployment", function(): void {
   this.timeout(5000);
 
-  var deploymentList;
+  var deploymentList: Deployment[];
 
   before(
     testUtils.setupKudu(false, function(kuduApi): void {
@@ -19,129 +20,84 @@ describe("deployment", function(): void {
     })
   );
 
-  before(function(done): void {
+  before(async function(): Promise<void> {
     this.timeout(30 * 1000);
 
-    api.deployment
-      .deployAsync(gitUrl1)
-      .then(function(): void {
-        return api.deployment.deployAsync(gitUrl2);
-      })
-      .then(function(): void {
-        return api.deployment.listAsync();
-      })
-      .then(function(result): void {
-        deploymentList = result.data;
-        done();
-      })
-      .catch(done);
+    await api.deployment.deploy(gitUrl1);
+    await api.deployment.deploy(gitUrl2);
+    const response = await api.deployment.list();
+    deploymentList = response.payload;
   });
 
-  it("can list all deployments", function(done): void {
-    api.deployment.list(function(err, result): void {
-      if (err) {
-        return done(err);
-      }
-
-      assert(Array.isArray(result.data), "Deployments should be an array");
-      done();
-    });
+  it("can list all deployments", async function(): Promise<void> {
+    const response = await api.deployment.list();
+    assert(Array.isArray(response.payload), "Deployments should be an array");
   });
 
-  it("can get a single deployment", function(done): void {
-    api.deployment.get(deploymentList[0].id, function(err, result): void {
-      if (err) {
-        return done(err);
-      }
+  it("can get a single deployment", async function(): Promise<void> {
+    const response = await api.deployment.get(deploymentList[0].id);
 
-      assert.equal(
-        result.data.id,
-        deploymentList[0].id,
-        "Deployment id should match the one queried"
-      );
-      done();
-    });
+    assert.equal(
+      response.payload.id,
+      deploymentList[0].id,
+      "Deployment id should match the one queried"
+    );
   });
 
-  it("can deploy a previous deployment", function(done): void {
+  it("can deploy a previous deployment", async function(): Promise<void> {
     this.timeout(30 * 1000);
 
-    api.deployment
-      .redeployAsync(deploymentList[0].id)
-      .then(function(): void {
-        return api.deployment.getAsync(deploymentList[0].id);
-      })
-      .then(function(result): void {
-        var deployment = result.data;
+    await api.deployment.redeploy(deploymentList[0].id);
 
-        assert.equal(
-          deployment.id,
-          deploymentList[0].id,
-          "Deployment id should match the one queried"
-        );
-        assert(deployment.active, "Deployment should be active");
-        done();
-      })
-      .catch(done);
+    const response = await api.deployment.get(deploymentList[0].id);
+    const deployment: Deployment = response.payload;
+    assert.equal(
+      deployment.id,
+      deploymentList[0].id,
+      "Deployment id should match the one queried"
+    );
+    assert(deployment.active, "Deployment should be active");
   });
 
-  it("can delete a deployment", function(done): void {
-    var oldDeployments;
+  it("can delete a deployment", async function(): Promise<void> {
+    const response = await api.deployment.list();
+    const initialDeployments = response.payload;
+    const deploymentId = initialDeployments.filter(function(d): boolean {
+      return !d.active;
+    })[0].id;
 
-    api.deployment
-      .listAsync()
-      .then(function(result): void {
-        oldDeployments = result.data;
-        var deploymentId = oldDeployments.filter(function(d): boolean {
-          return !d.active;
-        })[0].id;
+    await api.deployment.del(deploymentId);
 
-        return api.deployment.delAsync(deploymentId);
-      })
-      .then(function(): void {
-        return api.deployment.listAsync();
-      })
-      .then(function(result): void {
-        assert.equal(
-          result.data.length,
-          oldDeployments.length - 1,
-          "Deployment count should be one less after deletion"
-        );
-        done();
-      })
-      .catch(done);
+    const response2 = await api.deployment.list();
+    assert.equal(
+      response2.payload.length,
+      initialDeployments.length - 1,
+      "Deployment count should be one less after deletion"
+    );
   });
 
-  it("can get a deployment log", function(done): void {
-    api.deployment.log(deploymentList[0].id, function(err, result): void {
-      if (err) {
-        return done(err);
-      }
+  it("can get a deployment log", async function(): Promise<void> {
+    const response = await api.deployment.log(deploymentList[0].id);
 
-      assert(
-        Array.isArray(result.data),
-        "Deployment log entries should be an array"
-      );
-      done();
-    });
+    assert(
+      Array.isArray(response.payload),
+      "Deployment log entries should be an array"
+    );
   });
 
-  it("can get a deployment log entry", function(done): void {
+  it("can get a deployment log entry", async function(): Promise<void> {
     var deploymentId = deploymentList[0].id;
 
-    api.deployment
-      .logAsync(deploymentId)
-      .then(function(result): void {
-        var entry = result.data.filter(function(entry): boolean {
-          return !!entry.details_url;
-        })[0];
+    const response = await api.deployment.log(deploymentId);
 
-        return api.deployment.logDetailsAsync(deploymentId, entry.id);
-      })
-      .then(function(result): void {
-        assert(result.data);
-        done();
-      })
-      .catch(done);
+    var entry = response.payload.filter(function(entry): boolean {
+      return !!entry.details_url;
+    })[0];
+
+    const detailsResponse = await api.deployment.logDetails(
+      deploymentId,
+      entry.id
+    );
+    assert(detailsResponse.payload);
   });
 });
