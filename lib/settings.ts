dacfa098797e8658edb8bd268d45d211a10073be
Will Anderson <will@itsananderson.com>
2019-05-26 17:11:17 -0700

    Convert 'settings' API to native Promises

--- lib/settings.ts
+++ lib/settings.ts
@@ -1,48 +1,70 @@
 import * as utils from "./utils";
+import { RequestAPI, Request, CoreOptions, RequiredUriUrl } from "request";
+import { ApiResponse } from "./types";
 
 export interface Settings {
-  list: (cb) => void;
-  get: (key, cb) => void;
-  del: (key, cb) => void;
-  set: (settings, cb) => void;
+  list: () => Promise<ApiResponse<{ [key: string]: string }>>;
+  get: (key: string) => Promise<ApiResponse<string>>;
+  del: (key: string) => Promise<ApiResponse<void>>;
+  set: (settings: { [key: string]: string }) => Promise<ApiResponse<void>>;
 }
 
-export default function settings(request): Settings {
+export default function settings(
+  request: RequestAPI<Request, CoreOptions, RequiredUriUrl>
+): Settings {
   return {
-    list: function list(cb): void {
+    list: function list(): Promise<ApiResponse<{ [key: string]: string }>> {
       var options = {
         uri: "/api/settings",
         json: true
       };
 
-      request(options, utils.createCallback("listing settings", cb));
+      return new Promise<ApiResponse<{ [key: string]: string }>>(
+        (resolve, reject) => {
+          request(
+            options,
+            utils.createPromiseCallback("listing settings", resolve, reject)
+          );
+        }
+      );
     },
 
-    get: function get(key, cb): void {
+    get: function get(key: string): Promise<ApiResponse<string>> {
       var options = {
         uri: "/api/settings/" + encodeURIComponent(key),
         json: true
       };
       var action = "getting setting with key " + key;
 
-      request(options, utils.createCallback(action, cb));
+      return new Promise<ApiResponse<string>>((resolve, reject) => {
+        request(options, utils.createPromiseCallback(action, resolve, reject));
+      });
     },
 
-    del: function del(key, cb): void {
+    del: function del(key: string): Promise<ApiResponse<void>> {
       var url = "/api/settings/" + encodeURIComponent(key);
       var action = "deleting setting with key " + key;
 
-      request.del(url, utils.createCallback(action, cb));
+      return new Promise<ApiResponse<void>>((resolve, reject) => {
+        request.del(url, utils.createPromiseCallback(action, resolve, reject));
+      });
     },
 
-    set: function set(settings, cb): void {
+    set: function set(settings: {
+      [key: string]: string;
+    }): Promise<ApiResponse<void>> {
       var options = {
         uri: "/api/settings/",
         json: settings
       };
       var action = "updating settings";
 
-      request.post(options, utils.createCallback(action, cb));
+      return new Promise<ApiResponse<void>>((resolve, reject) => {
+        request.post(
+          options,
+          utils.createPromiseCallback(action, resolve, reject)
+        );
+      });
     }
   };
 }
